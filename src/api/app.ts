4721d9ea2a80b0c663b4fafbedf8f9d8975d5e35
ion22666 <ionmocanu2266@gmail.com>
2023-01-20 23:15:47 +0200

    a

--- src/api/app.ts
+++ src/api/app.ts
@@ -2,11 +2,15 @@ import express from "express";
 import middlewares from "./middlewares/middlewares";
 import appRouter from "./routes/app-router";
 import apiRouter from "./routes/api-router";
+import config from "./config/server-config";
 
 const app = express();
 
 // Define Front Middleware Functions
-middlewares.start.forEach(middleware => app.use(...middleware));
+middlewares.start.forEach((middleware, index) => {
+    if (config.mode == "development" && index == 0) return;
+    app.use(...middleware);
+});
 
 // Define routes
 app.use("/", appRouter);
