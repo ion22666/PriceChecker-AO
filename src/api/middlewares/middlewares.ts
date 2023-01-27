0f0a94f8b624e50be1b0fdfd244e1bb44fee1292
ion22666 <ionmocanu2266@gmail.com>
2023-01-27 19:39:41 +0200

    a

--- src/api/middlewares/middlewares.ts
+++ src/api/middlewares/middlewares.ts
@@ -5,10 +5,12 @@ import express from "express";
 import { Handler } from "express-serve-static-core";
 import config from "../config/server-config";
 
-export default {
+const middlawares: {
+    start: [string, Handler][];
+    end: [string, Handler][];
+} = {
     // !!! using "*" over "" will destroy the value of req.url
     start: [
-        ["", config.mode == "development" ? consoleLogging : void 0], // log to the console every request and also the response ( with a callback )
         ["", favicon], // return facivon.ico if a url ends in /facivon.ico
         ["", express.json()], // if a request has a body, try to parse it to JSON
         ["", express.urlencoded({ extended: true })], // if the request type is
@@ -17,7 +19,7 @@ export default {
         ["/", express.static("dist/assets")], // check if the request match a static file
         ["", Return404], // just send a 404 error if the request reach this point
     ],
-} as {
-    start: [string, Handler][];
-    end: [string, Handler][];
 };
+if (config.mode == "development") middlawares.start.unshift(["", consoleLogging]); // log to the console every request and also the response ( with a callback )
+
+export default middlawares;
